86d3b804cf92c9844b3a1988f1b7c6a8f1d021c5
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2024-01-01 14:11:45 +0600

    assignment 1 complete

---
+++ basics-assignment-1-problem/basics-assignment-1-problem/app.js
@@ -0,0 +1,14 @@
+const app = Vue.createApp({
+    data: function() {
+        return {
+            name: 'Md Sabbir Rahman',
+            age: 22,
+            imgUrl: 'https://picsum.photos/300'
+        }
+    },
+    methods: {
+
+    }
+})
+
+app.mount('#assignment')
